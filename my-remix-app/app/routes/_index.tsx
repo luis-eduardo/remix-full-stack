df09ec78e2393d44ec38c6b9c78749ee6f1754d6
luis-eduardo <luis-eduardo@users.noreply.github.com>
2025-07-14 17:50:10 -0400

    A

---
+++ my-remix-app/app/routes/_index.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default function Index() {
+  const [userAgent, setUserAgent] = useState('the World');
+
+  useEffect(() => {
+    setUserAgent(window.navigator.userAgent);
+  }, []);
+  
+  console.log(`Another hello to ${userAgent}`);
+  return (
+    <h1>Hello to {userAgent}!</h1>
+  );
+}
