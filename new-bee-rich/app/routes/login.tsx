5cf7475e27b32ef8becd156597873749d9af2a53
luis-eduardo <luis-eduardo@users.noreply.github.com>
2025-07-15 10:43:15 -0400

    Add Login and SignUp routes

---
+++ new-bee-rich/app/routes/login.tsx
@@ -0,0 +1,7 @@
+﻿import {H1} from "~/components/headings";
+
+export default function LoginPage() {
+    return (
+        <H1>Login page</H1>
+    )
+}
---
+++ new-bee-rich/app/routes/signup.tsx
@@ -0,0 +1,7 @@
+﻿import {H1} from "~/components/headings";
+
+export default function Component() {
+    return (
+        <H1>Sign up Page</H1>
+    )
+}
