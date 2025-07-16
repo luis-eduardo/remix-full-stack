21d7875b0aa9bf1fedffa80f101d2bff0b66a2cd
luis-eduardo <luis-eduardo@users.noreply.github.com>
2025-07-16 13:46:45 -0400

    Add loader function to dashboard to allow Remix to execute loader functions in parallel and avoid request waterfall.

--- new-bee-rich/app/routes/dashboard.tsx
+++ new-bee-rich/app/routes/dashboard.tsx
@@ -1,8 +1,46 @@
 ﻿import {Container} from "~/components/containers";
-import {Link as RemixLink, Outlet} from "@remix-run/react";
+import {Link as RemixLink, Outlet, useLoaderData, useLocation} from "@remix-run/react";
 import {NavLink} from "~/components/links";
+import {db} from "~/modules/db.server";
+
+export async function loader(){
+    const expenseQuery = db.expense.findFirst({orderBy: {createdAt: 'desc'}});
+    const invoiceQuery = db.invoice.findFirst({orderBy: {createdAt: 'desc'}});
+    
+    const [firstExpense, firstInvoice] = await Promise.all([expenseQuery, invoiceQuery]);
+    
+    return {
+        firstExpense,
+        firstInvoice,
+    };
+}
 
 export default function Component() {
+    const location = useLocation();
+    const loaderData = useLoaderData<typeof loader>();
+    
+    const expensesUrl = '/dashboard/expenses';
+    const expensesActive = location.pathname.startsWith(expensesUrl);
+        
+    const incomeUrl = '/dashboard/income';
+    const incomeActive = location.pathname.startsWith(incomeUrl);
+    
+    const getExpensesLinkTo = () => {
+        if (loaderData.firstExpense !== null) {
+            return `${expensesUrl}/${loaderData.firstExpense.id}`;
+        } else {
+            return expensesUrl;
+        }
+    }
+    
+    const getIncomeLinkTo = () => {
+        if(loaderData.firstInvoice !== null) {
+            return `${incomeUrl}/${loaderData.firstInvoice.id}`;
+        } else {
+            return incomeUrl;
+        }
+    }
+
     return (
         <>
             <header>
@@ -18,10 +56,10 @@ export default function Component() {
                         </ul>
                         <ul className="w-full flex flex-row gap-5 mt-10 clear-both">
                             <li className="ml-auto">
-                                <NavLink to="/dashboard/income">Income</NavLink>
+                                <NavLink to={getIncomeLinkTo()} styleAsActive={incomeActive}>Income</NavLink>
                             </li>
                             <li className="mr-auto">
-                                <NavLink to="/dashboard/expenses">Expenses</NavLink>
+                                <NavLink to={getExpensesLinkTo()} styleAsActive={expensesActive}>Expenses</NavLink>
                             </li>
                         </ul>
                     </nav>
