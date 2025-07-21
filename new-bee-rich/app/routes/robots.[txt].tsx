f32ac66961b5ec9c2e2b7a9a1dad440e57abadf4
luis-eduardo <luis-eduardo@users.noreply.github.com>
2025-07-20 21:42:35 -0400

    Moved robots.txt to a dynamic route handler.

---
+++ new-bee-rich/app/routes/robots.[txt].tsx
@@ -0,0 +1,12 @@
+﻿const textContent =
+`User-agent: *
+Disallow: /dashboard/
+Allow: /login
+Allow: /signup
+Allow: /$`;
+
+export function loader() {
+    return new Response(textContent, {
+        headers: { 'content-type': 'text/plain' }
+    });
+}
